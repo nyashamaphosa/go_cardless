c39074099235c2d60e463faf18e51891b1180e5a
nyashamaphosa <maphosan@kjnetadmins-MacBook-Pro.local>
2017-06-27 10:13:51 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoCardless::Application.config.secret_token = '379304260c041705701e9d9c63875c7ece748b6554f8b9f1ec0c26366080c88db07d86a2fa2da0b34aecee843ca46cc95e2acf0a3bda4658f0d4b75ef2d6ab3f'
